6100965a48098ac2d173d10b805430a37e58be3d
yeahiwon <89372405+yeahiwon@users.noreply.github.com>
2021-12-17 16:30:43 +0900

    create Meteor constrcutor

---
+++ meteor.js
@@ -0,0 +1,22 @@
+class Meteor {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.length = 15;
+    this.r = 0;
+    this.c = color(74, 168,random(0,216));
+    this.mass = 10;
+    this.opacity = 200;
+    }
+
+  show() {
+    noStroke();
+    fill(this.c);
+    ellipse(this.pos.x, this.pos.y, this.length);
+  }
+
+  update() {
+
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let meteor = [];
+
 function setup() {
   createCanvas(300, 700);
+
+  for(let i = 0; i < 10; i++) {
+  meteor[i] = new Meteor(random(0, 300),random(0, 700));
+}
 }
 
 function draw() {
   background(0);
+
+  for(let i=0; i < meteor.length; i++) {
+    meteor[i].show();
+    meteor[i].update();
+  }
 }
