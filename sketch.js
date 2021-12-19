d2c3ece3a4bc212ac49304a7c1f110a0da93a6f4
yeahiwon <89372405+yeahiwon@users.noreply.github.com>
2021-12-19 23:01:11 +0900

    fix background music code

--- sketch.js
+++ sketch.js
@@ -23,6 +23,7 @@ function preload() {
 function setup() {
 
   createCanvas(300, 700);
+  myUniverse.loop();
   myUniverse.play();
   for(let i = 0; i < 10; i++) {
   meteor[i] = new Meteor(random(0, 300),random(0, 700));
