4edc57e6fa1f269c34247d0830aa857200149bb6
yeahiwon <89372405+yeahiwon@users.noreply.github.com>
2021-12-19 13:51:15 +0900

    fix myplanet code itself

--- myplanet.js
+++ myplanet.js
@@ -1,3 +1,9 @@
 class Myplanet{
-  
+  constructor(){
+
+  }
+  show(){
+  }
+  update(){
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,3 +1,8 @@
+var cx = 150;
+var cy = 350;
+var cr = 50;
+var speed = 5;
+
 let meteor = [];
 
 function setup() {
@@ -10,9 +15,23 @@ function setup() {
 
 function draw() {
   background(0);
+  fill(255,0,0);
+  if (overCircle(cx,cy,cr)){
+  cx = cx + random(-speed,speed);
+  cy = cy + random(-speed,speed);
+  }
+  ellipse(cx,cy,cr*2,cr*2);
 
   for(let i=0; i < meteor.length; i++) {
     meteor[i].show();
     meteor[i].update();
+  }
+}
+
+function overCircle(x,y,r){
+  if (dist(x,y,mouseX,mouseY)<r){
+    return true;
+  } else {
+    return false;
   }
 }
